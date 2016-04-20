e8f8385a0e478debd6f2eee970b55b5ed35bd915
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2016-04-20 14:17:49 -0700

    From GitHub -> Travis -> Azure

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git rm -r fake-data/
+
 bundle install
 bundle exec jekyll build
---
+++ .travis/pre-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Deployments respect `.gitignore`, so _site wouldn't be deployed. We have travis find its
+# innerself with a git identity, force add the `_site/` directory, and commit it for it to
+# be pushed
+git config --global user.name "travis-ci"
+git config user.email "travis@localhost"
+git add -f _site/
+git commit --amend --no-edit
