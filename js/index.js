a4d3690427af688da9a506f7efc0357154f31613
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2016-04-27 12:33:49 -0700

    Use top pages instead of top domains
    closes #8

--- js/index.js
+++ js/index.js
@@ -311,7 +311,9 @@
       })
       .render(barChart()
         .label(function(d) { return d.page_title; })
-        .value(function(d) { return +d.active_visitors; })
+        .value(function(d) {
+          return (typeof d.visits === 'undefined') ? d.active_visitors : d.visits;
+        })
         .scale(function(values) {
           var max = d3.max(values);
           return d3.scale.linear()
