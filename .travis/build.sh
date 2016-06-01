32ee62809cf0d1673efddaf1a8791e3492f0db7e
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2016-06-01 13:15:35 -0700

    Move Python setup to build step for caching
    See #28

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,3 +5,7 @@ git rm -r fake-data/
 bundle install --path vendor/bundle
 bundle exec jekyll build
 #bundle exec htmlproofer ./_site
+
+# Get our Python dependencies
+python -m virtualenv _site/pyenv
+_site/pyenv/bin/pip install -r requirements.txt
