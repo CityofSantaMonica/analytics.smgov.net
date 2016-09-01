1e81363f0f8221b4886ba45cdda491f9ad19bbc4
allejo <vladimir.jimenez@smgov.net>
2016-09-01 16:36:53 -0700

    Have Travis deploy Python 3.4 instead of 2.7
    - Also throw in a sodapy update for kicks, and because it plays nice with Python 3.4
    - References #42

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,5 +7,6 @@ bundle exec jekyll build
 #bundle exec htmlproofer ./_site
 
 # Get our Python dependencies
+python --version
 python -m virtualenv _site/pyenv
 _site/pyenv/bin/pip install -r requirements.txt
